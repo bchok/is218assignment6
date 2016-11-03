382fd28aa53e0a9d786ce4c5f05f67a455fa68d8
bchok <bchok91@gmail.com>
2016-11-03 17:53:44 -0400

    added cpu usage functions and a couple statements to make it clearer what is being demonstrated

--- is218assignment6.php
+++ is218assignment6.php
@@ -1,4 +1,5 @@
 <?php
+    echo 'this demonstrates arbitrary number of arguments<br>';
     //function that can take any two arguments
     function foo($arg1 = '', $arg2 = ''){
         
@@ -22,7 +23,8 @@
     foo2();
     foo2('hello');
     foo2('hello', 'world', 'again');
-
+///////////////////////////////////////////////////////////////////////
+    echo 'this demonstrates the glob function<br>';
     //glob function that can find filesize
     //get all php files 
     $files = glob('*.php');
@@ -42,6 +44,23 @@
     //applies the function to each array element
     $files = array_map('realpath', $files);
     print_r($files);
+////////////////////////////////////////////////////////////////////////
+    echo 'This demonstrates the memory usage functions<br>';
+    echo "Initial: ".memory_get_usage()." bytes \n";
+
+    //lets use up some memory 
+    for($i = 0; $i < 100000; $i++){
+        unset($array[$i]);
+    }
+
+    echo "Final: ".memory_get_usage()." bytes \n";
+    echo "Peak: ".memory_get_peak_usage()." bytes \n";
+///////////////////////////////////////////////////////////////////////////
+    echo 'This demonstrates the cpu usage functions<br>';
+    
+
+
+
 
 
 
