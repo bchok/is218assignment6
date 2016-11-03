07e113c84ff7626f31aa272d5034e70e202f3373
bchok <bchok91@gmail.com>
2016-11-03 18:12:12 -0400

    added missing cpu examplesfrom previous commit

--- is218assignment6.php
+++ is218assignment6.php
@@ -59,10 +59,36 @@
     echo 'This demonstrates the cpu usage functions<br>';
     print_r(getrusage());
 
+    //sleep for 3 seconds (non-busy)
+    sleep(3);
+    $data = getrusage();
+    echo "User time: ".
+        ($data['ru_utime.tv_sec'] + $data['ru_utime.tv_usec'] / 1000000);
+    echo "System time: ".
+        ($data['ru_stime.tv_sec'] + $data['ru_stime.tv_usec'] / 1000000);
 
 
+    //loop 10 million times (busy)
+    for($i = 0; $i < 10000000; $i++){
 
+    }    
+    $data = getrusage();
+    echo "User time: ".
+        ($data['ru_utime.tv_sec'] + $data['ru_utime.tv_usec'] / 1000000);
+    echo "System time: ". 
+        ($data['ru_stime.tv_sec'] + $data['ru_stime.tv_usec'] / 1000000);
+    
+    $start = microtime(true);
+    //keep calling microtime for about 3 seconds
+    while(microtime(true) - $start < 3){
 
+    }
+    $data = getrusage();
+    echo "User time: ". 
+        ($data['ru_utime.tv_sec'] + $data['ru_utime.tv_usec'] / 1000000);
+    echo "System time: ". 
+        ($data['ru_stime.tv_sec'] + $data['ru_stime.tv_usec'] / 1000000);
+    
 
 
 ?>
